9e32d1f8dee433e1f2bbf32f3ac526932f94d96f
ramplerrsky <arsnijusepenko@mail.ru>
2024-10-09 19:28:40 +0600

    1

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('learnMore').addEventListener('click', function() {
-    alert('Host Code — это бот, который позволяет управлять вашим хостингом через Telegram. Вы можете получать уведомления, управлять настройками и многое другое!');
-});
